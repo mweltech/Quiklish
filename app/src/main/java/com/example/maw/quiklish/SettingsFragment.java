b526500dd1cde7fe6f9545356b9b3455c55e9bc5
HairyDot <livewire123!>
2015-02-06 16:27:27 +1100

    Mike initial version

---
+++ app/src/main/java/com/example/maw/quiklish/SettingsFragment.java
@@ -0,0 +1,22 @@
+package com.example.maw.quiklish;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+
+    public class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+
+        }
+
+    }
